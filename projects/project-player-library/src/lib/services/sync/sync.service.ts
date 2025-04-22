8d21fd26015c5deb4f64d4de403e117996b624a0
Vinod-V3 <vinod@tunerlabs.com>
2025-04-23 00:19:53 +0530

    Generic code changes

--- projects/project-player-library/src/lib/services/sync/sync.service.ts
+++ projects/project-player-library/src/lib/services/sync/sync.service.ts
@@ -120,7 +120,7 @@ export class SyncService {
           headers: {
             "Content-Type": "multipart/form-data",
             "Access-Control-Allow-Origin":"*",
-            ...(fileDetails.cloudStorage === "azure" ? { "x-ms-blob-type": "BlockBlob" } : {})
+            ...(fileDetails.cloudStorage.toLowerCase() === "azure" ? { "x-ms-blob-type": "BlockBlob" } : {})
           }
         }
         firstValueFrom(this.customHttp.put(fileDetails.uploadUrl, convertedFile, options)).then(data=>{
